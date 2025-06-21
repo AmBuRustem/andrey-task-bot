b29d0bddc7c3f36f8f04a704c776ed017f75fd26
AmBuRustem <RusChamidov@gmail.com>
2025-06-22 01:31:11 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,2 +1,18 @@
 # Основной файл бота
 print('Бот Андрей запущен')
+# Внизу файла bot.py
+from flask import Flask
+import threading
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "Бот Андрей работает"
+
+def run_flask():
+    app.run(host="0.0.0.0", port=10000)
+
+# Запуск Flask в отдельном потоке
+flask_thread = threading.Thread(target=run_flask)
+flask_thread.start()
